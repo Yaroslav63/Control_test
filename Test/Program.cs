f2971c108954a37671e9030521238253dcda74e8
Yaroslav63 <era6300@mail.ru>
2022-05-08 21:15:41 +0300

    add print

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,2 +1,9 @@
 ﻿string[] array = new string[4] { "123", "var", "-123", "asdf" };
 string[] arr = new string[array.Length];
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}" + " ");
+    }
+}
